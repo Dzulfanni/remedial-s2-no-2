782bf0083124bafc7bbede784f6f0e430271e000
Dzulfanni <belajarayok866@gmail.com>
2020-09-17 09:47:09 +0700

    first commit

---
+++ no2.js
@@ -0,0 +1,7 @@
+var nilaiSantri = [ 100, 20, 35, 70, 99, 80, 100, 97, 84 ];
+var total= 0;
+var jumlahNilai=nilaiSantri.length;
+for(i=0; i<jumlahNilai; i++){
+    total+=nilaiSantri[i]
+}
+document.write(total/jumlahNilai)
